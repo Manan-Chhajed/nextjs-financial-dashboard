30cb2498df5aa21356d4ce37c933f3506ce39e60
Manan-Chhajed <mananhemant288@gmail.com>
2024-08-18 12:52:25 +0530

    add features to create update delete invoices

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Page() {
             className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
           />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to BillMaster.</strong> One stop solution for all your invoicing needs.{' '}
+            <a href="https://github.com/Manan-Chhajed/nextjs-financial-dashboard" className="text-blue-500">
+              Checkout source code here
             </a>
-            , brought to you by Vercel.
+            .
           </p>
           <Link
             href="/login"
@@ -32,6 +32,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
+            // src="/hero.jpg"
             src="/hero-desktop.png"
             width={1000}
             height={760}
@@ -39,6 +40,7 @@ export default function Page() {
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
+            // src="/hero.jpg"
             src="/hero-mobile.png"
             width={560}
             height={620}
